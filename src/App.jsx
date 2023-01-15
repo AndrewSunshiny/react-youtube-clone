eabdb4923f24c04e2ef23ef304818c5fcd3b976b
AndrewSunshiny <50839774+AndrewSunshiny@users.noreply.github.com>
2023-01-16 02:50:51 +0300

    fix react-router double routers

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,17 @@ import { Box } from '@mui/material'
 import { BrowserRouter, Route, Routes, HashRouter } from 'react-router-dom'
 
 const App = () => (
-  <BrowserRouter>
-    <HashRouter basename="/">
-      <Box sx={{ backgroundColor: '#000' }}>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Feed />}></Route>
-          <Route path="/video/:id" element={<VideoDetail />}></Route>
-          <Route path="/channel/:id" element={<ChannelDetail />}></Route>
-          <Route path="/search/:searchItem" element={<SearchFeed />}></Route>
-        </Routes>
-      </Box>
-    </HashRouter>
-  </BrowserRouter>
+  <HashRouter basename="/">
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />}></Route>
+        <Route path="/video/:id" element={<VideoDetail />}></Route>
+        <Route path="/channel/:id" element={<ChannelDetail />}></Route>
+        <Route path="/search/:searchItem" element={<SearchFeed />}></Route>
+      </Routes>
+    </Box>
+  </HashRouter>
 )
 
 export default App
